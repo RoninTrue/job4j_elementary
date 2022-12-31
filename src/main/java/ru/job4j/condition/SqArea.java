821bf1069d7fb4b0d890a70b15011bfbb1b1e776
RoninTrue <vasenev.front@gmail.com>
2022-12-31 17:24:41 +0700

    4.5. Отладка программы в IDEA [#159464 #376406]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -1,11 +1,11 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(double p, double k) {
-        double h = p / (2 * (k + 1));
-        double i = h * k;
-        double rsl = i * h;
-        return rsl;
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = h * k;
+        int s = w * h;
+        return s;
     }
 
     public static void main(String[] args) {
--- src/test/java/ru/job4j/condition/SqAreaTest.java
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -1,7 +1,8 @@
 package ru.job4j.condition;
 
-import org.junit.Test;
-import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 public class SqAreaTest {
 
@@ -9,26 +10,17 @@ public class SqAreaTest {
     public void whenP6K2Square2() {
         int expected = 2;
         int p = 6;
-        double k = 2;
+        int k = 2;
         double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 
     @Test
-    public void whenP8K3Square3() {
-        int expected = 3;
-        int p = 8;
-        double k = 3;
+    public void whenP4K1Square1() {
+        int expected = 1;
+        int p = 4;
+        int k = 1;
         double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void whenP12K5Square5() {
-        int expected = 5;
-        int p = 12;
-        double k = 5;
-        double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
+        assertThat(out).isCloseTo(expected, offset(0.01));
     }
 }
