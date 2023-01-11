f8d999c251a6b6aee8db4f11f6922cd78010a225
RoninTrue <vasenev.front@gmail.com>
2023-01-11 20:05:56 +0700

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -1,40 +1,34 @@
 package ru.job4j.condition;
 
-import org.junit.Test;
-import  org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import  static org.assertj.core.api.Assertions.assertThat;
 
 public class PointTest {
 
     @Test
     public void when00to20then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        double result = a.distance(b);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void when10to40then3() {
+        Point a = new Point(1, 0);
+        Point b = new Point(4, 0);
+        double result = a.distance(b);
         double expected = 3;
-        int x1 = 1;
-        int y1 = 0;
-        int x2 = 4;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void when01to09then8() {
+        Point a = new Point(0, 1);
+        Point b = new Point(0, 9);
+        double result = a.distance(b);
         double expected = 8;
-        int x1 = 0;
-        int y1 = 1;
-        int x2 = 0;
-        int y2 = 9;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        assertThat(result).isEqualTo(expected);
     }
 }
